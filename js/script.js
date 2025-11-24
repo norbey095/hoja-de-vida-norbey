f1e4870d40f0fbda048bddd7958ad673cd6b285e
norbey095 <norbey095@gmail.com>
2025-11-23 19:56:10 -0500

    modificacion de hoja de vida

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ enlaces.forEach(enlace => {
 
 const CERTIFICADOS = [
   { titulo: "Curso Java Básico - Platzi 2024", archivo: "assets/certificados/diploma-programacion-basica.pdf" },
-  { titulo: "Bootcamp FullStack Angular & Java - PRAGMAPRAGMA 2024", archivo: "assets/certificados/Certificado-power-Up-norbey.pdf" },
+  { titulo: "Bootcamp FullStack Angular & Java - PRAGMAPRAGMA 2024", archivo: "assets/certificados/certificado-power-Up-norbey.pdf" },
   { titulo: "Curso de Inteligencia artifical (IA)- Sapienza medellin 2025", archivo: "assets/certificados/certificado-IA-norbey.pdf" }
 ];
 
